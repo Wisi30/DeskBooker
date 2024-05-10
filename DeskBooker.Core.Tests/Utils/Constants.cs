bebc03d90eb99bac028ae0cfda7d8a08d34e7c87
wisi30 <wisi30@hotmail.es>
2024-05-10 12:33:13 +0100

    chore: move to adapt file structure

--- DeskBooker.Core.Tests/Utils/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DeskBooker.Core.Tests.Utils
-{
-    public class Constants
-    {
-        // Exceptions
-        public static string InvalidEmailMessage = "El correo electrónico no es válido";
-    }
-}
